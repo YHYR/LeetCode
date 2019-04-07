7d86731237f09a6f72f286040e5d9b720f7ecc7b
yhyr <yhyr_ycy@163.com>
2019-04-07 10:03:58 +0800

    type: fix; subject: 优化Q1

--- src/com/yhyr/Algorithm/Q1_TwoSum.java
+++ src/com/yhyr/Algorithm/Q1_TwoSum.java
@@ -1,5 +1,8 @@
 package com.yhyr.Algorithm;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Subject:
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
@@ -9,7 +12,32 @@ package com.yhyr.Algorithm;
  * @since 2019/01/04 15:21
  */
 public class Q1_TwoSum {
-    private int[] twoSum(int[] nums, int target) {
+    /**
+     * 基于散列表实现
+     * 时间复杂度O(n)
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];
+            if (map.keySet().contains(temp)) {
+                return new int[]{map.get(temp), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+
+    /**
+     * 第一次提交结果 => 性能最差
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum_1(int[] nums, int target) {
         int[] result = new int[2];
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
