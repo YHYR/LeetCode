4668fe90791c7ba6b5867c07885485360c314f93
haochen10 <haochen10@iflytek.com>
2019-09-15 11:34:11 +0800

    type: feat; subject: Accept Q17

---
+++ src/com/yhyr/Algorithm/Q17_LetterCombination.java
@@ -0,0 +1,50 @@
+package com.yhyr.Algorithm;
+
+import java.util.*;
+
+/**
+ * @author yhyr
+ * @since 2019/09/15 11:00
+ */
+public class Q17_LetterCombination {
+    private static Map<Character, List<String>> numLetterMapping = new HashMap<Character, List<String>>() {
+        {
+            put('2', Arrays.asList("a", "b", "c"));
+            put('3', Arrays.asList("d", "e", "f"));
+            put('4', Arrays.asList("g", "h", "i"));
+            put('5', Arrays.asList("j", "k", "l"));
+            put('6', Arrays.asList("m", "n", "o"));
+            put('7', Arrays.asList("p", "q", "r", "s"));
+            put('8', Arrays.asList("t", "u", "v"));
+            put('9', Arrays.asList("w", "x", "y", "z"));
+        }
+    };
+
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+        char[] chars = digits.toCharArray();
+        if (chars.length == 0) {
+            return result;
+        }
+        result = numLetterMapping.get(chars[0]);
+        for (int i = 1; i < chars.length; i++) {
+            result = combination(result, numLetterMapping.get(chars[i]));
+        }
+        return result;
+    }
+
+    private List<String> combination(List<String> list1, List<String> list2) {
+        List<String> result = new ArrayList<>();
+        for (String s1 : list1) {
+            for (String s2 : list2) {
+                result.add(s1 + s2);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Q17_LetterCombination action = new Q17_LetterCombination();
+        List<String> result = action.letterCombinations("234");
+    }
+}
