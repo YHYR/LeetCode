dea078965f51d921256e57f8fa46915a60803f1c
haochen10 <haochen10@iflytek.com>
2019-09-28 01:48:48 +0800

    type: fix; subject: Q62新增非递归解法

--- src/com/yhyr/Algorithm/Q62_UniquePaths.java
+++ src/com/yhyr/Algorithm/Q62_UniquePaths.java
@@ -9,26 +9,57 @@ public class Q62_UniquePaths {
         if (m == 0 || n == 0) {
             return 0;
         }
-        return findPath(m, n);
+        return findPathByRecursive(m, n);
     }
 
     /**
-     * 递归
+     * 基于递归的动态规划
      *
      * 思路简单，代码实现也很精简，但是提交超时；因为递归反复压栈出栈太耗时
+     * 
      * @param m
      * @param n
      * @return
      */
-    private int findPath(int m, int n) {
+    private int findPathByRecursive(int m, int n) {
         if (m == 1 || n == 1) {
             return 1;
         }
-        return findPath(m - 1, n) + findPath(m, n - 1);
+        // return findPathByRecursive(m - 1, n) + findPathByRecursive(m, n - 1);
+        return findPathByIterator(m, n);
+    }
+
+
+    /**
+     * 将递归式动态规划改写成迭代
+     * 动态规划公示：f(m, n) = f(m-1)(n) + f(m)(n-1)
+     * 以3*3为例，左下角坐标为(0,0)，则从原点出发，向左向上值均为1，其他节点按照公式进行计算
+     *
+     * 1,1,1
+     * 1,2,3
+     * 1,3,6
+     * @param m
+     * @param n
+     * @return
+     */
+    private int findPathByIterator(int m, int n) {
+        int[][] result = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            result[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            result[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                result[i][j] = result[i - 1][j] + result[i][j - 1];
+            }
+        }
+        return result[m - 1][n - 1];
     }
 
     public static void main(String[] args) {
         Q62_UniquePaths action = new Q62_UniquePaths();
-        System.out.println(action.uniquePaths(3, 3));
+        System.out.println(action.uniquePaths(3, 2));
     }
 }
